a04f29471a65817d524b1cf4e6480597ce5962a5
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-14 00:19:19 +0000

    Create cb.js

---
+++ functions/cb.js
@@ -0,0 +1,17 @@
+//CallBack Function
+function exibir(num) {
+    console.log(`A operação foi ${num}`)
+}
+
+function soma(a, b, cb) {
+    let op = a + b
+    cb(op)
+}
+
+function multiplicação(a, b, cb) {
+    let op = a * b
+    let(op)
+}
+
+soma(2, 4, exibir)
+multiplicação(4, 6, exibir)
