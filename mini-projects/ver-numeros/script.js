fb65c7bf77a49df1071eb798f4607acefd012355
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-24 14:33:49 +0000

    Update script.js

--- mini-projects/ver-numeros/script.js
+++ mini-projects/ver-numeros/script.js
@@ -1,68 +1,68 @@
-let add = document.querySelector('#but1')
-let fin = document.querySelector('#but2')
+let add = document.querySelector("#but1");
+let fin = document.querySelector("#but2");
 
-add.addEventListener("click", addValores)
-fin.addEventListener("click", finValores)
+add.addEventListener("click", addValores);
+fin.addEventListener("click", finValores);
 
-let num = document.querySelector('#fnum')
-let lista = document.querySelector('#flista')
-let res = document.querySelector('#res')
-let valores = []
+let num = document.querySelector("#fnum");
+let lista = document.querySelector("#flista");
+let res = document.querySelector("#res");
+let valores = [];
 
-function isNumero(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
-        return true
-    }else{
-        return false
-    }
+function isNumero(n) {
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function inLista (n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true
-    } else {
-        return false
-    }
+function inLista(n, l) {
+  if (l.indexOf(Number(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function addValores(){
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
-        res.innerHTML = ''
-    } else {
-        alert ('Valor inválido ou ja encontrado na lista.')
-    }
-    num.value = ''
-    num.focus()
+function addValores() {
+  if (isNumero(num.value) && !inLista(num.value, valores)) {
+    valores.push(Number(num.value));
+    let item = document.createElement("option");
+    item.text = `Valor ${num.value} adicionado.`;
+    lista.appendChild(item);
+    res.innerHTML = "";
+  } else {
+    alert("Valor inválido ou ja encontrado na lista.");
+  }
+  num.value = "";
+  num.focus();
 }
 
-function finValores(){
-    if(valores == 0) {
-        alert ('Adicione valores antes de finalilzar!')
-    } else {
-        let tot = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
-        let media = 0
-        let soma = 0
+function finValores() {
+  if (valores == 0) {
+    alert("Adicione valores antes de finalilzar!");
+  } else {
+    let tot = valores.length;
+    let maior = valores[0];
+    let menor = valores[0];
+    let media = 0;
+    let soma = 0;
 
-        for (let pos in valores) {
-            soma += valores[pos]
-            if(valores[pos] > maior) {
-                maior = valores[pos]
-            } else {
-                menor = valores[pos]
-            }
-        }
-        media = soma / tot
-        res.innerHTML = ''
-        res.innerHTML += `Ao todo temos ${tot} números registados <br>`
-        res.innerHTML += `O maior número adicionado foi ${maior} <br>`
-        res.innerHTML += `O menor número adicionado foi ${menor} <br>`
-        res.innerHTML += `A soma dos valores é ${soma} <br>`
-        res.innerHTML += `A média dos valores é ${media} <br>`
+    for (let pos in valores) {
+      soma += valores[pos];
+      if (valores[pos] > maior) {
+        maior = valores[pos];
+      } else {
+        menor = valores[pos];
+      }
     }
+    media = soma / tot;
+    res.innerHTML = "";
+    res.innerHTML += `Ao todo temos ${tot} números registados <br>`;
+    res.innerHTML += `O maior número adicionado foi ${maior} <br>`;
+    res.innerHTML += `O menor número adicionado foi ${menor} <br>`;
+    res.innerHTML += `A soma dos valores é ${soma} <br>`;
+    res.innerHTML += `A média dos valores é ${media} <br>`;
+  }
 }
